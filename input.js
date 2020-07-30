eb9e9b50a588531e9386be03899021259367cc74
Andrew Garnett <andrewgarnett@Andrews-Mac-mini.local>
2020-07-29 22:29:09 -0400

    input.js

---
+++ input.js
@@ -0,0 +1,7 @@
+const prompt = require('prompt-sync')();
+
+const name = prompt('What is your name?');
+console.log(`Hey there ${name}`);
+
+const color= prompt('What is your color?');
+console.log(`My favorite color is ${color}`);
