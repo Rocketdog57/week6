71bf3d2ae3c972ee31be06e4dfebcb37989bbe08
Andrew Garnett <andrewgarnett@Andrews-Mac-mini.local>
2020-07-30 23:16:57 -0400

    server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var http = require('http');
 var url = require('url');  
 var fs = require('fs');
 
+//a web server so that when a browser navigates to "localhost:8080/about" 
+
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
     var myReadStream = fs.createReadStream(__dirname + '/about.html');
