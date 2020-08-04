bba0a16692b653e971dfe865dd905b0b83dec7db
Andrew Garnett <andrewgarnett@Andrews-Mac-mini.local>
2020-08-04 12:56:31 -0400

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,54 @@
 var http = require('http');
-var url = require('url');  
 var fs = require('fs');
+var path = require('path');
 
-//a web server so that when a browser navigates to "localhost:8080/about" 
+http.createServer(function (request, response) {
+    console.log('request ', request.url);
 
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '/about.html');
-	res.write("About");
-	res.end();
-  }).listen(8080);
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './about.html';
+    }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT') {
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(404, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8080);
+console.log('Server running at http://127.0.0.1:8080/');
